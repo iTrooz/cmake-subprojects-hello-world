cd8f81d4e2f0da738d905563e3c9d08cf449abdc
iTrooz <hey@itrooz.fr>
2023-05-12 20:03:26 +0200

    add project

---
+++ README.md
@@ -0,0 +1,13 @@
+Simple example of how to use CMake with subprojects
+
+Compilation:
+```
+<go in the project directory>
+mkdir build
+cd build
+cmake ..
+make
+./exe1
+```
+
+Licence: CC0/no licence
---
+++ exe1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "lib1.h"
+#include "lib2.h"
+
+int main(){
+    std::cout << "Hey" << std::endl;
+    std::cout << add(2, 3) << std::endl;
+    std::cout << multiply(2, 3) << std::endl;
+}
